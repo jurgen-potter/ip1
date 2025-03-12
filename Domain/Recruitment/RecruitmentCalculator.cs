be696b03af0367fb6c2ca11093cce84e0e1c97dd
larzzzy <lars.veringmeier@student.kdg.be>
2025-03-12 16:03:43 +0100

    Start issue 26

---
+++ Domain/Recruitment/RecruitmentCalculator.cs
@@ -0,0 +1,38 @@
+﻿namespace CitizenPanel.BL.Domain.Recruitment;
+
+public class RecruitmentCalculator
+{
+    // Bevat de berekening voor het aantal te rekruteren panelleden.
+    public RecruitmentResult CalculateRecruitment(RecruitmentCriteria criteria)
+    {
+        var TotalAvailablePotPanelmembers = 0.424 * double.Sqrt(criteria.TotalAvailablePotentialPanelmembers); //veranderen van plaats
+        
+        var result = new RecruitmentResult
+        {
+           
+            MaleCount = (int)(TotalAvailablePotPanelmembers * (criteria.MalePercentage / 100)),
+            FemaleCount = (int)(TotalAvailablePotPanelmembers * (criteria.FemalePercentage / 100)),
+            Age18_25Count = (int)(TotalAvailablePotPanelmembers * (criteria.Age18_25Percentage / 100)),
+            Age26_40Count = (int)(TotalAvailablePotPanelmembers * (criteria.Age26_40Percentage / 100)),
+            Age41_60Count = (int)(TotalAvailablePotPanelmembers * (criteria.Age41_60Percentage / 100)),
+            Age60PlusCount = (int)(TotalAvailablePotPanelmembers * (criteria.Age60PlusPercentage / 100)),
+            ExtraCriteriaResults = new List<CriteriaResult>()
+        };
+
+        foreach (var extra in criteria.ExtraCriteria)
+        {
+            var criteriaResult = new CriteriaResult
+            {
+                Name = extra.Name,
+                SubResults = extra.SubCriteria.Select(sub => new SubCriteriaResult
+                {
+                    Name = sub.Name,
+                    Count = (int)(TotalAvailablePotPanelmembers * (sub.Percentage / 100))
+                }).ToList()
+            };
+            result.ExtraCriteriaResults.Add(criteriaResult);
+        }
+
+        return result;
+    }
+}
---
+++ Domain/Recruitment/RecruitmentCriteria.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CitizenPanel.BL.Domain.Recruitment;
+
+public class RecruitmentCriteria : IValidatableObject
+{
+    //Bevat de gewenste percentages per doelgroep.
+    public int TotalAvailablePotentialPanelmembers { get; set; }
+
+    public double MalePercentage { get; set; }
+    public double FemalePercentage { get; set; }
+
+    public double Age18_25Percentage { get; set; }
+    public double Age26_40Percentage { get; set; }
+    public double Age41_60Percentage { get; set; }
+    public double Age60PlusPercentage { get; set; }
+
+    public List<ExtraCriteria> ExtraCriteria { get; set; } = new List<ExtraCriteria>();
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (MalePercentage + FemalePercentage != 100)
+            yield return new ValidationResult("Het totaal van Male en Female moet precies 100% zijn.");
+
+        if (Age18_25Percentage + Age26_40Percentage + Age41_60Percentage + Age60PlusPercentage != 100)
+            yield return new ValidationResult("Het totaal van de leeftijdsgroepen moet precies 100% zijn.");
+
+        foreach (var criteria in ExtraCriteria)
+        {
+            if (criteria.SubCriteria.Sum(s => s.Percentage) != 100)
+                yield return new ValidationResult($"De subcategorieën van '{criteria.Name}' moeten samen 100% zijn.");
+        }
+    } 
+}
