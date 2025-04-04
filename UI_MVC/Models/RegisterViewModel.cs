d7a4afe5c7a982a1f7ebe8ccafda3a43c290a858
Xander Verbruggen <xander.verbruggen@student.kdg.be>
2025-04-04 11:09:52 +0200

    Validatie van de criteria toegevoegd

--- UI_MVC/Models/RegisterViewModel.cs
+++ UI_MVC/Models/RegisterViewModel.cs
@@ -1,8 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 using CitizenPanel.BL.Domain.Draw;
 
 namespace CitizenPanel.UI.MVC.Models;
 
-public class RegisterViewModel
+public class RegisterViewModel : IValidatableObject
 {
     public List<ExtraCriteria> CriteriaList { get; set; }
     
@@ -12,4 +13,14 @@ public class RegisterViewModel
     
     public bool IsConfirmed { get; set; }
 
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        List<ValidationResult> errors = new List<ValidationResult>();
+        if (SelectedCriteria == null || SelectedCriteria.Count < CriteriaList.Count)
+        {
+            errors.Add(new ValidationResult("Please select all criteria", new[] { nameof(SelectedCriteria) }));
+            
+        }
+        return errors;
+    }
 }
