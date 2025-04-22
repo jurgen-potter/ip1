96f055b8227d65630e3897a96b9d0b4c2173ab71
Lars Veringmeier <lars.veringmeier@student.kdg.be>
2025-04-22 14:59:26 +0200

    aangepast nu echt naar TS

--- UI_MVC/main.ts
+++ UI_MVC/main.ts
@@ -2,6 +2,6 @@
 import './wwwroot/js/recommendation/vote.ts';
 import './wwwroot/js/recommendation/votersModalLoader.ts';
 import './wwwroot/js/account/login.js';
-import './wwwroot/js/memberRegister/registerMember.js';
+import './wwwroot/js/memberRegister/registerMember';
 import './wwwroot/js/registration/index.js';
 
