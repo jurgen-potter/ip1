f8385ff4ff90d359058ece4d8e10284df9e08fc5
Jurgen.P <jurgen.potter@student.kdg.be>
2025-04-22 11:31:57 +0200

    Javascript omgezet naar typescript

---
+++ UI_MVC/wwwroot/js/account/login.ts
@@ -0,0 +1,52 @@
+﻿window.addEventListener('DOMContentLoaded', () => {
+    loginInit();
+});
+
+function loginInit(): void {
+    const selectedLogin = document.getElementById("selectedLogin") as HTMLInputElement;
+    let selectedType = selectedLogin?.value ?? "Account";
+
+    let selectedBtn = document.getElementById("accountBtn") as HTMLButtonElement;
+
+    if (selectedType !== "Account") {
+        selectedType = "Code";
+        selectedBtn = document.getElementById("codeBtn") as HTMLButtonElement;
+    }
+
+    selectedBtn?.classList.add("active");
+    showCorrectFields(selectedType);
+    setEventListener();
+}
+
+function showCorrectFields(selectedType: string): void {
+    const accountFields = document.getElementById("accountFields") as HTMLDivElement;
+    const codeFields = document.getElementById("codeFields") as HTMLDivElement;
+    const selectedLogin = document.getElementById("selectedLogin") as HTMLInputElement;
+
+    if (accountFields && codeFields && selectedLogin) {
+        if (selectedType === "Account") {
+            codeFields.classList.add("d-none");
+            accountFields.classList.remove("d-none");
+            selectedLogin.value = "PanelMember";
+        } else {
+            codeFields.classList.remove("d-none");
+            accountFields.classList.add("d-none");
+            selectedLogin.value = "Organization";
+        }
+    }
+}
+
+function setEventListener(): void {
+    const loginTypeButtons = document.querySelectorAll<HTMLButtonElement>(".login-type-btn");
+
+    loginTypeButtons.forEach(button => {
+        button.addEventListener("click", function (this: Element) {
+            loginTypeButtons.forEach(btn => btn.classList.remove("active"));
+
+            this.classList.add("active");
+            const selectedType = this.getAttribute("data-type") ?? "Account";
+
+            showCorrectFields(selectedType);
+        });
+    });
+}
