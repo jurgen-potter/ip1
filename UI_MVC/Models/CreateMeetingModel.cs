3b1a0fe4b176cb43aa6abb39a8c099e7b3730005
larzzzy <lars.veringmeier@student.kdg.be>
2025-05-02 17:49:04 +0200

    Modal scherm gevalideerd en meetings worden nu op de juiste plaats gezet na het aanmaken! Einde issue

---
+++ UI_MVC/Models/CreateMeetingModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CitizenPanel.UI.MVC.Models;
+
+public class CreateMeetingModel : IValidatableObject
+{
+    
+    public int PanelId { get; set; }
+
+    [Required(ErrorMessage = "Vul een titel in")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "Selecteer een datum")]
+    [DataType(DataType.Date, ErrorMessage = "Ongeldige datumnotatie")]
+    public DateOnly Date { get; set; }
+
+    
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Date < DateOnly.FromDateTime(DateTime.Now))
+        {
+            yield return new ValidationResult(
+                "De datum mag niet in het verleden liggen.",
+                new[] { nameof(Date) });
+        }
+    }
+}
