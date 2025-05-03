ef847d82ec7c57e90585e41a2d2b6ab7f4c84ee2
Xander Verbruggen <xander.verbruggen@student.kdg.be>
2025-05-03 17:21:12 +0200

    EndVote

---
+++ UI_MVC/wwwroot/js/recommendation/endVote.ts
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let selectedRecommendationId: number | null = null;
+
+    const confirmationModal = document.getElementById("confirmationModal") as HTMLElement;
+    const stopVotingButton = confirmationModal?.querySelector(".btn-danger") as HTMLButtonElement;
+    const waitButton = confirmationModal?.querySelector(".btn-success") as HTMLButtonElement;
+
+    const endVoteButtons = document.querySelectorAll('.btn-stop-voting')
+    
+    endVoteButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const recommendationId = (button as HTMLElement).getAttribute("data-recommendation-id");
+            if (recommendationId) {
+                selectedRecommendationId = parseInt(recommendationId);
+            }
+        });
+    });
+
+    stopVotingButton?.addEventListener("click", async () => {
+        if (selectedRecommendationId === null) return;
+
+        fetch(`/Recommendation/StopVoting/${selectedRecommendationId}`, {
+            method: 'POST'
+        })
+            .then(res => {
+                if (!res.ok) {
+                    throw Error(`Received status code ${res.status}.`)
+                }
+            })
+            .catch(err => alert('Something went wrong: ' + err));
+
+        endVoteButtons.forEach(button => {
+            const recommendationId = (button as HTMLElement).getAttribute("data-recommendation-id");
+            if (recommendationId && parseInt(recommendationId) === selectedRecommendationId) {
+                (button as HTMLButtonElement).hidden = true;
+            }
+        })
+
+        const modalInstance = bootstrap.Modal.getInstance(confirmationModal);
+        modalInstance?.hide();
+        selectedRecommendationId = null;
+    });
+
+    waitButton?.addEventListener("click", () => {
+        const modalInstance = bootstrap.Modal.getInstance(confirmationModal);
+        modalInstance?.hide();
+        selectedRecommendationId = null;
+    });
+});
