7732409e2c430e822fa22412f46a09ec902464f1
Jurgen.P <jurgen.potter@student.kdg.be>
2025-05-03 16:01:36 +0200

    Kleine fix na merge

--- UI_MVC/Controllers/RecruitmentController.cs
+++ UI_MVC/Controllers/RecruitmentController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
 
 namespace CitizenPanel.UI.MVC.Controllers;
 
