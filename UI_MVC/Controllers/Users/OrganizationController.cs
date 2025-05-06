4a561b44d9a4688d7c1b33588739bba8c6bdcac6
Jurgen.P <jurgen.potter@student.kdg.be>
2025-05-06 10:56:37 +0200

    Paul kan geen org overzicht meer zien. rip Paul

--- UI_MVC/Controllers/Users/OrganizationController.cs
+++ UI_MVC/Controllers/Users/OrganizationController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CitizenPanel.UI.MVC.Controllers.Users;
 
 public class OrganizationController : Controller
 {
     [HttpGet]
+    [Authorize (Roles = "Organization")]
     public IActionResult Index()
     {
         return View();
