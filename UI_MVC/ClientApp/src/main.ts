9d2932c4529acfef2de637a99098dfdedad816af
Lars Veringmeier <lars.veringmeier@student.kdg.be>
2025-05-13 14:08:15 +0200

    fixed typescript stacking in eachother! changed files import and stuffs

--- UI_MVC/ClientApp/src/main.ts
+++ UI_MVC/ClientApp/src/main.ts
@@ -1,19 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
 import './index.scss';
 import './styles/admin/editQuestionnaire.scss';
 import './styles/recruitment/index.scss';
 
 
-import './ts/recommendation/endVote.ts';
-import './ts/recommendation/vote.ts';
-import './ts/recommendation/votersModalLoader';
-import './ts/dashboard/timeline.ts';
-import './ts/recruitment/index.ts';
-import './ts/registration/index.ts';
-import './ts/memberRegister/registerMember.ts';
-import './ts/admin/editQuestionnaire.ts';
-import './ts/account/login.ts';
-
-createApp(App).mount('#app') 
+// import './ts/recommendation/endVote.ts';
+// import './ts/recommendation/vote.ts';
+// import './ts/recommendation/votersModalLoader';
+// import './ts/dashboard/timeline.ts';
+// import './ts/recruitment/index.ts';
+// import './ts/registration/index.ts';
+// import './ts/memberRegister/registerMember.ts';
+// import './ts/admin/editQuestionnaire.ts';
+// import './ts/account/login.ts';
--- UI_MVC/ClientApp/vite.config.ts
+++ UI_MVC/ClientApp/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [],
   server: {
     proxy: {
       '/api': {
@@ -16,11 +15,24 @@ export default defineConfig({
     outDir: '../wwwroot/dist/clientapp',
     assetsDir: 'assets',
     rollupOptions: {
+      input: {
+        main: path.resolve('src/main.ts'),
+        styles: path.resolve('src/index.scss'),
+        vote: path.resolve('src/ts/recommendation/vote.ts'),
+        endVote: path.resolve('src/ts/recommendation/endVote.ts'),
+        votersModalLoader: path.resolve('src/ts/recommendation/votersModalLoader.ts'),
+        dashboard: path.resolve('src/ts/dashboard/timeline.ts'),
+        recruitment: path.resolve('src/ts/recruitment/index.ts'),
+        registration: path.resolve('src/ts/registration/index.ts'),
+        memberRegister: path.resolve('src/ts/memberRegister/registerMember.ts'),
+        admin: path.resolve('src/ts/admin/editQuestionnaire.ts'),
+        account: path.resolve('src/ts/account/login.ts')
+      },
       output: {
         assetFileNames: 'assets/[name][extname]',
         entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js'
+        chunkFileNames: 'assets/[name]-[hash].js'
       }
     }
   }
-}) 
+})
