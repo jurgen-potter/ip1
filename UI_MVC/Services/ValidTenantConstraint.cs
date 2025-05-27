5484910acd4c3b599e90fb54f77eb7e45d454dff
Xander Verbruggen <xander.verbruggen@student.kdg.be>
2025-05-27 11:02:00 +0200

    comments uit ValidTenantConstraint gehaald

--- UI_MVC/Services/ValidTenantConstraint.cs
+++ UI_MVC/Services/ValidTenantConstraint.cs
@@ -5,26 +5,6 @@ namespace CitizenPanel.UI.MVC.Services;
 
 public class ValidTenantConstraint : IRouteConstraint
 {
-    /*
-    private readonly IServiceProvider _serviceProvider;
-
-    public ValidTenantConstraint(IServiceProvider serviceProvider)
-    {
-        _serviceProvider = serviceProvider;
-    }
-
-    public bool Match(HttpContext httpContext, IRouter route, string routeKey,
-        RouteValueDictionary values, RouteDirection routeDirection)
-    {
-        if (!values.TryGetValue(routeKey, out var value) || value is not string tenantId)
-            return false;
-
-        // Use a scoped service provider to get the DbContext
-        using var scope = _serviceProvider.CreateScope();
-        var db = scope.ServiceProvider.GetRequiredService<PanelDbContext>();
-
-        return db.Tenants.Any(t => t.Id == tenantId);
-    }*/
     private readonly TenantStore _tenantStore;
 
     public ValidTenantConstraint(TenantStore tenantStore)
