10a00f9958cf18ef15b1befacab74c7858bfc51d
larzzzy <lars.veringmeier@student.kdg.be>
2025-05-28 17:50:22 +0200

    Identity pages gefixed

--- UI_MVC/Program.cs
+++ UI_MVC/Program.cs
@@ -125,7 +125,7 @@ using (var scope = app.Services.CreateScope())
 
 using (IServiceScope scope = app.Services.CreateScope()) {
     PanelDbContext context = scope.ServiceProvider.GetRequiredService<PanelDbContext>();
-    if (context.CreateDatabase(true)) {
+    if (context.CreateDatabase(false)) {
         
         var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
         var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
