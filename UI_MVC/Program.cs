0b3fcb9a6f4259bf00d0689321bc2e01d88d2092
larzzzy <lars.veringmeier@student.kdg.be>
2025-05-28 20:28:17 +0200

    alles buiten edit pagina zou cv moeten zijn

--- UI_MVC/Program.cs
+++ UI_MVC/Program.cs
@@ -156,7 +156,7 @@ using (var scope = app.Services.CreateScope())
 
 using (IServiceScope scope = app.Services.CreateScope()) {
     PanelDbContext context = scope.ServiceProvider.GetRequiredService<PanelDbContext>();
-    if (context.CreateDatabase(true)) {
+    if (context.CreateDatabase(false)) {
         var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
         var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
         IdentitySeeder identitySeeder = new IdentitySeeder(userManager, roleManager);
